a27fc5daff85955807cf397e7d7caa33f115427c
mdondio <ronaldo4>
2016-02-16 23:45:51 +0100

    Integration of Watson capabilities

--- ServiceInterfaces/src/com/it/ibm/watson/frontendapp/CVSettingsPanel.java
+++ ServiceInterfaces/src/com/it/ibm/watson/frontendapp/CVSettingsPanel.java
@@ -5,31 +5,55 @@ import javax.swing.JPanel;
 import javax.swing.JScrollPane;
 import javax.swing.JTextArea;
 
+import com.ibm.watson.developer_cloud.document_conversion.v1.DocumentConversion;
+import com.ibm.watson.developer_cloud.personality_insights.v2.PersonalityInsights;
+import com.ibm.watson.developer_cloud.personality_insights.v2.model.Profile;
+import com.it.ibm.watson.cvassesment.DocumentConversionConfig;
+import com.it.ibm.watson.cvassesment.PersonalityInsightConfig;
+
 import java.awt.Dimension;
 import java.awt.GridBagConstraints;
 import java.awt.GridBagLayout;
 import java.awt.Insets;
 import java.awt.event.*;
+import java.io.BufferedWriter;
 import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.PrintWriter;
 
 public class CVSettingsPanel extends JPanel implements ActionListener {
 
 	public javax.swing.JTextField inputFolder;
 	public javax.swing.JButton inputBrowser;
 	public javax.swing.JLabel inputLabel;
-	public javax.swing.JLabel logLabel;	
-	
+	public javax.swing.JLabel logLabel;
+
 	public javax.swing.JTextField outputFolder;
 	public javax.swing.JButton outputBrowser;
 	public javax.swing.JLabel outputLabel;
-	
+
 	public javax.swing.JButton startAnalysis;
 	public javax.swing.JTextArea log;
-	
+
 	JFileChooser fc;
-	
-	public CVSettingsPanel(){
-		
+
+	// Watson services
+	private DocumentConversion service;
+	private PersonalityInsights pi;
+
+	public CVSettingsPanel() {
+
+		// Initialize (hardcoded)
+		service = new DocumentConversion(
+				DocumentConversion.VERSION_DATE_2015_12_01);
+		service.setUsernameAndPassword(DocumentConversionConfig.usernameAndrea,
+				DocumentConversionConfig.passwordAndrea);
+
+		pi = new PersonalityInsights();
+		pi.setUsernameAndPassword(PersonalityInsightConfig.usernameAndrea,
+				PersonalityInsightConfig.passwordAndrea);
+
 		// Setting all the variables of this class
 		inputFolder = new javax.swing.JTextField();
 		outputFolder = new javax.swing.JTextField();
@@ -39,7 +63,7 @@ public class CVSettingsPanel extends JPanel implements ActionListener {
 		outputLabel = new javax.swing.JLabel("Output directory path");
 		logLabel = new javax.swing.JLabel("Log panel");
 		startAnalysis = new javax.swing.JButton("START");
-		
+
 		fc = new JFileChooser();
 
 		inputBrowser.addActionListener(this);
@@ -48,22 +72,21 @@ public class CVSettingsPanel extends JPanel implements ActionListener {
 
 		inputFolder.setPreferredSize(new Dimension(600, 25));
 		outputFolder.setPreferredSize(new Dimension(600, 25));
-	
-		log = new JTextArea(5,20);
-        log.setMargin(new Insets(5,5,5,5));
-        log.setEditable(false);
-        
+
+		log = new JTextArea(5, 20);
+		log.setMargin(new Insets(5, 5, 5, 5));
+		log.setEditable(false);
+
 		// Formatting this panel (TO BE EXTENED with multiple layout choices)
 		formatLayout();
-		
-		
+
 	}
-		
+
 	public void actionPerformed(ActionEvent e) {
-		
+
 		// Handle open button action.
 		if (e.getSource() == inputBrowser) {
-			fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
+			// fc.setFileSelectionMode(JFileChooser.DIRECTORIES_ONLY);
 			int returnVal = fc.showOpenDialog(CVSettingsPanel.this);
 			if (returnVal == JFileChooser.APPROVE_OPTION) {
 				File file = fc.getSelectedFile();
@@ -83,15 +106,40 @@ public class CVSettingsPanel extends JPanel implements ActionListener {
 				log.append("ERROR: the directory path is not valid.\n");
 			}
 		}
-		
+
 		if (e.getSource() == startAnalysis) {
-			log.append("WARNING: Still missing the link to the backend code.\n");
-			log.append("input folder:" + inputFolder.getText() + "\n");
-			log.append("output folder:" + outputFolder.getText() + "\n");
+			// log.append("WARNING: Still missing the link to the backend code.\n");
+			// log.append("input folder:" + inputFolder.getText() + "\n");
+			// log.append("output folder:" + outputFolder.getText() + "\n");
+
+			// Convert to txt
+			String normalizedText = service.convertDocumentToText(new File(
+					inputFolder.getText()));
+
+			// Now, we send this text to personality insight service
+			// log.append(normalizedText);
+
+			Profile personalityProfile = pi.getProfile(normalizedText);
+
+			log.append(personalityProfile.toString());
+
+			// Print to file
+			PrintWriter out = null;
+			try {
+				out = new PrintWriter(new BufferedWriter(new FileWriter(
+						outputFolder.getText() + "\\out.txt")));
+			} catch (IOException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+			out.println(personalityProfile);
+			out.close();
+
 		}
 
 	}
-	
+
 	public void formatLayout() {
 
 		this.setLayout(new GridBagLayout());
@@ -120,17 +168,15 @@ public class CVSettingsPanel extends JPanel implements ActionListener {
 		c.gridx = 0;
 		c.gridy = 5;
 		this.add(logLabel, c);
-		
+
 		c.gridx = 0;
 		c.gridy = 6;
 		this.add(logScrollPane, c);
-		
+
 		c.gridx = 1;
 		c.insets = new Insets(10, 1, 10, 1);
 		this.add(startAnalysis, c);
 
-
 	}
-
 
 }
--- ServiceInterfaces/src/com/it/ibm/watson/frontendapp/ServiceCatalog.java
+++ ServiceInterfaces/src/com/it/ibm/watson/frontendapp/ServiceCatalog.java
@@ -1,50 +1,57 @@
 package com.it.ibm.watson.frontendapp;
 
-import java.awt.event.ActionEvent;
 import java.awt.event.*;
+
 import javax.swing.*;
+
+import com.ibm.watson.developer_cloud.document_conversion.v1.DocumentConversion;
+import com.ibm.watson.developer_cloud.personality_insights.v2.PersonalityInsights;
+import com.it.ibm.watson.cvassesment.DocumentConversionConfig;
+import com.it.ibm.watson.cvassesment.PersonalityInsightConfig;
+
 import java.awt.*;
 
-
 public class ServiceCatalog extends JPanel implements ActionListener {
-	
+
 	private JButton CVassessment;
 	private JButton FaceRecognition;
 	private JButton WatsonSesdr;
 	private JButton IoTexperiment;
-	
+
 	private GlobalPanel CVservice;
 	private GlobalPanel FaceRecon;
 	private GlobalPanel Watson;
 	private GlobalPanel IoTexp;
-	
-	public ServiceCatalog(){
-		
+
+
+	public ServiceCatalog() {
+
+
 		CVassessment = new JButton();
 		CVassessment.setText("CV assessment service");
 		CVassessment.addActionListener(this);
-		CVassessment.setPreferredSize(new Dimension(500,50));
-				
+		CVassessment.setPreferredSize(new Dimension(500, 50));
+
 		FaceRecognition = new JButton();
 		FaceRecognition.setText("Face Recognition service");
 		FaceRecognition.addActionListener(this);
-		FaceRecognition.setPreferredSize(new Dimension(500,50));
-		
+		FaceRecognition.setPreferredSize(new Dimension(500, 50));
+
 		WatsonSesdr = new JButton();
 		WatsonSesdr.setText("Watson SESDR service");
 		WatsonSesdr.addActionListener(this);
-		WatsonSesdr.setPreferredSize(new Dimension(500,50));
-		
+		WatsonSesdr.setPreferredSize(new Dimension(500, 50));
+
 		IoTexperiment = new JButton();
 		IoTexperiment.setText("IoT for everyone service");
 		IoTexperiment.addActionListener(this);
-		IoTexperiment.setPreferredSize(new Dimension(500,50));
-		
+		IoTexperiment.setPreferredSize(new Dimension(500, 50));
+
 		formatLayout();
 	}
-	
-	public void formatLayout(){
-		
+
+	public void formatLayout() {
+
 		this.setLayout(new GridBagLayout());
 		GridBagConstraints c = new GridBagConstraints();
 
@@ -61,50 +68,50 @@ public class ServiceCatalog extends JPanel implements ActionListener {
 		this.add(WatsonSesdr, c);
 		c.gridx = 1;
 		this.add(IoTexperiment, c);
-		
+
 	}
-	
+
 	// Determine action of panel buttons
 	public void actionPerformed(ActionEvent e) {
 
 		if (e.getSource() == CVassessment) {
 			GlobalPanel CVservice = new GlobalPanel(0);
-		    javax.swing.SwingUtilities.invokeLater(new Runnable() {
-		        public void run() {
-		             CVservice.createAndShowGUI(0);
-		        }
-		    });
+			javax.swing.SwingUtilities.invokeLater(new Runnable() {
+				public void run() {
+					CVservice.createAndShowGUI(0);
+				}
+			});
 		}
-		
+
 		if (e.getSource() == FaceRecognition) {
 			GlobalPanel FaceRecon = new GlobalPanel(1);
 			javax.swing.SwingUtilities.invokeLater(new Runnable() {
-		        public void run() {
-		             FaceRecon.createAndShowGUI(1);
-		        }
-		    });
+				public void run() {
+					FaceRecon.createAndShowGUI(1);
+				}
+			});
 		}
-		
+
 		if (e.getSource() == WatsonSesdr) {
 			GlobalPanel Watson = new GlobalPanel(2);
 			javax.swing.SwingUtilities.invokeLater(new Runnable() {
-		        public void run() {
-		             Watson.createAndShowGUI(2);
-		        }
-		    });
+				public void run() {
+					Watson.createAndShowGUI(2);
+				}
+			});
 		}
-		
+
 		if (e.getSource() == IoTexperiment) {
 			GlobalPanel IoTexp = new GlobalPanel(3);
 			javax.swing.SwingUtilities.invokeLater(new Runnable() {
-		        public void run() {
-		             IoTexp.createAndShowGUI(3);
-		        }
-		    });
+				public void run() {
+					IoTexp.createAndShowGUI(3);
+				}
+			});
 		}
 
 	}
-	
+
 	/**
 	 * Create the GUI and show it. For thread safety, this method should be
 	 * invoked from the event-dispatching thread.
